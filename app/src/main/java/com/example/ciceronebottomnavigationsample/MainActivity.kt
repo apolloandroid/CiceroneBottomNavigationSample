ac29ed577b19faf72ad9e147b0b13ffd430b1b90
yu.khmelevskikh <yu.khmelevskikh@robo.cash>
2023-01-03 23:44:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ciceronebottomnavigationsample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ciceronebottomnavigationsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

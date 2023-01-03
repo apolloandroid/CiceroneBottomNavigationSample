db1e539d4ff61dfdd6c7169311759317ac779425
yu.khmelevskikh <yu.khmelevskikh@robo.cash>
2023-01-04 00:05:07 +0300

    Added fragments

---
+++ app/src/main/java/com/example/ciceronebottomnavigationsample/b/B1Fragment.kt
@@ -0,0 +1,18 @@
+package com.example.ciceronebottomnavigationsample.b
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ciceronebottomnavigationsample.R
+
+class B1Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_b1, container, false)
+    }
+}

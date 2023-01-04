2297b42f7604c82b6b13d316755949fd1472d6a3
yu.khmelevskikh <yu.khmelevskikh@robo.cash>
2023-01-04 19:41:41 +0300

    Added bottom navigation

--- app/src/main/java/com/example/ciceronebottomnavigationsample/MainActivity.kt
+++ app/src/main/java/com/example/ciceronebottomnavigationsample/MainActivity.kt
@@ -2,11 +2,45 @@ package com.example.ciceronebottomnavigationsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ciceronebottomnavigationsample.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
+        binding.navigationView.apply {
+            setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.menuItemA -> selectTab("A")
+                    R.id.menuItemB -> selectTab("B")
+                    R.id.menuItemC -> selectTab("C")
+                }
+                false
+            }
+
+            setOnItemReselectedListener { item ->
+                when (item.itemId) {
+                    R.id.menuItemA -> selectTab("A")
+                    R.id.menuItemB -> selectTab("B")
+                    R.id.menuItemC -> selectTab("C")
+                }
+            }
+        }
+    }
+
+    private fun selectTab(title: String) {
+
     }
 }
+
